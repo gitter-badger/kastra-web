661a154d7d07084b2bca9ea55592dce1b4675769
Babat <babat42@gmail.com>
2018-11-06 22:44:59 +0100

    Fix home template issues

---
+++ src/Models/Template/HomeTemplateViewModel.cs
@@ -0,0 +1,11 @@
+using Kastra.Core.ViewComponents;
+
+namespace Kastra.Web.Models.Template
+{
+    public class HomeTemplateViewModel
+    {
+        public ModuleDataComponent Body { get; set; }
+        public ModuleDataComponent Header { get; set; }
+        public ModuleDataComponent Footer { get; set; }
+    }
+}
