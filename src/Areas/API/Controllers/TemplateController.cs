d0d84b67eb320a31722efed3b190324368b63de7
Babat <babat42@gmail.com>
2019-05-12 20:17:45 +0200

    Add page template management

--- src/Areas/API/Controllers/TemplateController.cs
+++ src/Areas/API/Controllers/TemplateController.cs
@@ -1,4 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Kastra.Core;
 using Kastra.Core.Business;
 using Kastra.Core.Dto;
 using Kastra.Web.API.Models.Template;
@@ -12,10 +17,24 @@ namespace Kastra.Web.Areas.API.Controllers
 	public class TemplateController : Controller
     {
         private readonly IViewManager _viewManager;
+        private readonly string[] _excludedFolders = new string[] { "Shared", "Install", "Account", "Manage" };
 
 		public TemplateController(IViewManager viewManager)
         {
             _viewManager = viewManager;
+        }
+
+        [HttpGet]
+        public IActionResult Get(int id)
+        {
+			TemplateInfo template = _viewManager.GetPageTemplate(id);
+
+			if (template == null)
+            {
+                return NotFound();
+            }
+
+			return Json(ToTemplateModel(template));
         }
 
         [HttpGet]
@@ -29,7 +48,78 @@ namespace Kastra.Web.Areas.API.Controllers
 				templateModels.Add(ToTemplateModel(template));
             }
 
-            return Json(templateModels);
+            return Ok(templateModels);
+        }
+
+        [HttpGet]
+        public IActionResult ListViewPaths()
+        {
+            var controllers = GetControllerTypes()
+                                .Select(c => c.Name.Replace("Controller", String.Empty))
+                                .Select(c => new { Name = c, Value = c });
+
+            return Ok(controllers);
+        }
+
+        [HttpGet]
+        public IActionResult ListKeynames(string viewPath)
+        {
+            TypeInfo controllerType = GetControllerTypes()
+                                        .SingleOrDefault(c => c.Name.Replace("Controller", String.Empty) == viewPath);
+
+            if (controllerType == null)
+            {
+                return BadRequest("Forbidden view path.");
+            }
+
+            // Get all template available for the selected controller
+            string viewDirectoryPath = Path.Combine(Directory.GetCurrentDirectory(), "Views", controllerType.Name.Replace("Controller", String.Empty));
+            var templates = Directory.GetFiles(viewDirectoryPath)
+                            .Select(c => Path.GetFileName(c).Replace(".cshtml", String.Empty))
+                            .Select(c => new { Name = c, Value = c });
+
+            return Ok(templates);
+        }
+
+        [HttpPost]
+        public IActionResult Update([FromBody]TemplateModel model)
+        {
+            TemplateInfo template = null;
+            
+            if (model.Id > 0)
+            {
+                template = _viewManager.GetPageTemplate(model.Id);
+            }
+
+            if (template == null)
+            {
+                template = new TemplateInfo();
+            }
+
+            template.Name = model.Name;
+            template.KeyName = model.KeyName;
+            template.ModelClass = model.ModelClass;
+            template.ViewPath = model.ViewPath.Replace("..", String.Empty);
+
+            _viewManager.SavePageTemplate(template);
+
+            return Ok(new { template.TemplateId });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete([FromBody]int id)
+        {
+            if (_viewManager.GetPageTemplate(id) == null)
+            {
+                return BadRequest("Template not found");
+            }
+
+            if (_viewManager.DeletePageTemplate(id))
+            {
+                return Ok();
+            }
+
+            return BadRequest();
         }
         
         #region Private methods
@@ -39,8 +129,34 @@ namespace Kastra.Web.Areas.API.Controllers
             TemplateModel model = new TemplateModel();
 			model.Id = templateInfo.TemplateId;
 			model.Name = templateInfo.Name;
+            model.KeyName = templateInfo.KeyName;
+            model.ModelClass = templateInfo.ModelClass;
+            model.ViewPath = templateInfo.ViewPath;
 
             return model;
+        }
+
+        /// <summary>
+        /// Get all template controllers
+        /// </summary>
+        /// <returns></returns>
+        public IList<TypeInfo> GetControllerTypes()
+        {
+            Type templateType = typeof(Kastra.Core.Controllers.TemplateController);
+            IList<TypeInfo> types = new List<TypeInfo>();
+
+            foreach(Assembly assembly in KastraAssembliesContext.Instance.Assemblies.Values)
+            {
+                foreach (TypeInfo typeInfo in assembly.DefinedTypes)
+                {
+                    if (typeInfo.IsSubclassOf(templateType))
+                    {
+                        types.Add(typeInfo);
+                    }
+                }
+            }
+
+            return types;
         }
 
         #endregion
--- src/Areas/API/Models/Template/TemplateModel.cs
+++ src/Areas/API/Models/Template/TemplateModel.cs
@@ -4,5 +4,8 @@
 	{
 		public int Id { get; set; }
 		public string Name { get; set; }
-    }
+		public string KeyName { get; set; }
+		public string ModelClass { get; set; }
+		public string ViewPath { get; set; }
+	}
 }
